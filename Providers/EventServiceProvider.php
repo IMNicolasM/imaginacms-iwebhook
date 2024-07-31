3132f0c7c8d36ac87c017da1d9543db33fc39d15
msolanogithub <msolano@imaginacolombia.com>
2024-07-31 14:33:55 -0500

    fixed event syntax

--- Providers/EventServiceProvider.php
+++ Providers/EventServiceProvider.php
@@ -8,11 +8,12 @@ use Modules\Webhook\Listeners\GenericEventWebhooks;
 
 class EventServiceProvider extends ServiceProvider
 {
-    public function boot()
-    {
-        parent::boot();
+  public function boot()
+  {
+    parent::boot();
 
-        Event::listen('eloquent.created: *', [GenericEventWebhooks::class, 'handle']);
-        Event::listen('eloquent.updated: *', [GenericEventWebhooks::class, 'handle']);
-        Event::listen('eloquent.deleted: *', [GenericEventWebhooks::class, 'handle']);
-    }
+    Event::listen('eloquent.created: *', [GenericEventWebhooks::class, 'handle']);
+    Event::listen('eloquent.updated: *', [GenericEventWebhooks::class, 'handle']);
+    Event::listen('eloquent.deleted: *', [GenericEventWebhooks::class, 'handle']);
+  }
+}
